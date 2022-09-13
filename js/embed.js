d5addb7153443a4bccfe50cf720655d4cc4664ca
c-cal <github-2c7c@zebrina.fr>
2022-09-13 09:39:01 +0200

    add embed.js

---
+++ js/embed.js
@@ -0,0 +1,170 @@
+/**
+ * @copyright Copyright (c) 2021, Watcha <contact@watcha.fr>
+ *
+ * @author Charlie Calendre <c-cal@watcha.fr>
+ *
+ * @license GNU AGPL version 3 or any later version
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Affero General Public License as
+ * published by the Free Software Foundation, either version 3 of the
+ * License, or (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ * GNU Affero General Public License for more details.
+ *
+ * You should have received a copy of the GNU Affero General Public License
+ * along with this program. If not, see <http://www.gnu.org/licenses/>.
+ *
+ */
+
+"use strict";
+
+function embed() {
+    const params = new URLSearchParams(window.location.search);
+    const embed = params.get("embed-watcha");
+    if (embed === "file-explorer" || window.name === "watcha-file-explorer") {
+        window.name = "watcha-file-explorer";
+        embedFileExplorer();
+    } else if (embed === "widget" || window.name === "watcha-widget") {
+        window.name = "watcha-widget";
+        const isCurrentChunck = chunck =>
+            new RegExp(`^${OC.webroot}(/index.php)?/${chunck}`).test(window.location.pathname);
+        const isCurrentApp = appName => isCurrentChunck("apps/" + appName);
+        if (isCurrentApp("files")) {
+            embedFilesWidget();
+        } else if (isCurrentApp("calendar")) {
+            embedCalendarWidget();
+        } else if (isCurrentApp("tasks")) {
+            embedTasksWidget();
+        } else if (isCurrentChunck("s/[A-Za-z0-9]+?")) {
+            embedDirectLinkFilesWidget();
+        }
+    }
+}
+
+function embedFileExplorer() {
+    _hideFilesToolbar();
+    const style = `
+        #filelist-header,
+        .app-sidebar,
+        #app-sidebar,
+        #view-toggle,
+        #headerSelection,
+        #headerSize,
+        .selection,
+        .fileactions,
+        .filesize {
+            display: none !important;
+        }
+
+        #body-user {
+            height: 100% !important;
+        }
+
+        #filestable > thead {
+            top: 44px !important;
+        }
+        
+        #app-content {
+            transform: none !important;
+        }`;
+    _injectStyle(style);
+}
+
+function embedFilesWidget() {
+    _hideFilesToolbar();
+}
+
+function embedCalendarWidget() {
+    const style = `
+        #header {
+            display: none !important;
+        }
+
+        #app-navigation-vue {
+            top: 0 !important;
+        }
+
+        #content-vue {
+            padding-top: 0 !important;
+        }`;
+    _injectStyle(style);
+}
+
+function embedTasksWidget() {
+    const style = `
+        #header {
+            display: none !important;
+        }
+
+        #app-navigation-vue,
+        .header {
+            top: 0 !important;
+        }
+
+        #content-vue {
+            padding-top: 0 !important;
+        }`;
+    _injectStyle(style);
+}
+
+function embedDirectLinkFilesWidget() {
+    const style = `
+        #header {
+            justify-content: center !important;
+            background-color: var(--color-main-background) !important;
+            background-image: none !important;
+        }
+
+        #header > .header-left,
+        #header-secondary-action {
+            display: none !important;
+        }
+
+        #header-primary-action > a {
+            background-color: rgb(0, 179, 179) !important;
+            border-color: rgb(0, 179, 179) !important;
+            border-radius: 8px !important;
+        }`;
+    _injectStyle(style);
+}
+
+function _hideFilesToolbar() {
+    const style = `
+        #header {
+            display: none !important;
+        }
+
+        #app-navigation,
+        #controls {
+            top: 0 !important;
+        }
+
+        #content {
+            padding-top: 0 !important;
+        }`;
+    _injectStyle(style);
+}
+
+function _injectStyle(style) {
+    let element = document.createElement("style");
+    element.innerHTML = style;
+    document.head.appendChild(element);
+}
+
+function postUrl(prevUrl) {
+    const url = window.location.href;
+    if (prevUrl && url !== prevUrl) {
+        window.parent.postMessage(url, OC.appConfig.watcha?.origin || "");
+    }
+    // HACK: to detect URLSearchParams changes that do not trigger a page reload
+    setTimeout(() => {
+        postUrl(url);
+    }, 200);
+}
+
+embed();
+postUrl();

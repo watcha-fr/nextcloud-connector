9e94fb0996084058294e830ac2b8f2e3b511ac5b
c-cal <github-2c7c@zebrina.fr>
2022-10-06 10:00:25 +0200

    build: bundle wrapper.js

--- webpack.config.js
+++ webpack.config.js
@@ -1,3 +1,9 @@
+const path = require('path')
 const webpackConfig = require('@nextcloud/webpack-vue-config')
+
+webpackConfig.entry = {
+	embed: path.join(__dirname, 'src', 'embed.js'),
+	wrapper: path.join(__dirname, 'src', 'wrapper.js'),
+}
 
 module.exports = webpackConfig
